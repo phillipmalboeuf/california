c914e73e0a77aa12cc87f5fd40875254da83f5f8
Phillip Malboeuf <phil@phils.computer>
2025-01-16 11:10:40 -0500

    cart sections

---
+++ naturals/src/lib/Cart.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let section: string
+
+  onMount(() => {
+    fetch('/cart?sections=cart&sections_url=%5C/cart')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.cart)
+        section = data.cart
+      })
+  })
+</script>
+
+{@html section}
--- naturals/src/main.ts
+++ naturals/src/main.ts
@@ -1,11 +1,14 @@
 import { mount } from 'svelte'
 import './app.scss'
 import App from './App.svelte'
+import Cart from './lib/Cart.svelte'
 
 mount(App, { target: document.getElementById('app')! })
+mount(Cart, { target: document.getElementById('cart-render')! })
 
 // const app = mount(App, {
 //   target: document.getElementById('app')!,
 // })
 
 // export default app
+
